ac8059751f8e17568fe02f261c377fe6f90eefe9
X <xac923@outlook.com>
2022-07-03 12:07:14 -0700

    added some gradient styling to bgc elemenets

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,7 +6,6 @@ import cert from '../../assets/cert.png'
 const About = () => {
   return (
     <div className='a'>
-          <h1 className="a-title">About Me</h1>
         <div className="a-left">
            <div className="a-card bg"></div> 
            <div className="a-card">
@@ -17,11 +16,12 @@ const About = () => {
             </div> 
         </div>
         <div className="a-right">
+            <h1 className="a-title">About Me</h1>
             <p className="a-sub">
                 After a decade of experience in Facilities and Energy Management, Xavier felt something calling to him.  His life-long curiosity with computers, technology, programming, and just outright creating things from thin air, fueling his pursuit toward a new direction. 
             </p>
             <p className="a-desc">
-                Always looking to learn and nurture his creativity - Xavier enrolled in a Software Engineering bootcamp with General Assembly, looking to turn his passion into an occupation. 
+                Always looking to learn and nurture his creativity - Xavier enrolled in a Software Engineering boot camp with General Assembly, looking to turn his passion into an occupation. 
             </p>
             <div className="a-award">
                 <img 
